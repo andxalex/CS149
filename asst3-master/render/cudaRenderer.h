1a1e9ce2295cd8e05a9cef43737d1b2f58715780
andreas <andreas.so.alexandrou@gmail.com>
2024-11-07 14:07:42 -0800

    moved macro to cu file

--- asst3-master/render/cudaRenderer.h
+++ asst3-master/render/cudaRenderer.h
@@ -6,23 +6,6 @@
 #endif
 
 #include "circleRenderer.h"
-#include <cuda_runtime.h>
-#define DEBUG
-
-#ifdef DEBUG
-#define cudaCheckError(ans) { cudaAssert((ans), __FILE__, __LINE__); }
-inline void cudaAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
-   if (code != cudaSuccess)
-   {
-      fprintf(stderr, "CUDA Error: %s at %s:%d\n",
-        cudaGetErrorString(code), file, line);
-      if (abort) exit(code);
-   }
-}
-#else
-#define cudaCheckError(ans) ans
-#endif
 
 class CudaRenderer : public CircleRenderer {
 
